2dd955c46c85c05b488ad63d850529dd33c4ed9c
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-02-19 10:08:12 +0100

    add variables css and reset string

--- wp-content/themes/aurum-child/functions.php
+++ wp-content/themes/aurum-child/functions.php
@@ -6,9 +6,9 @@
  *	www.laborator.co
  */
 
-// This function will enqueue style.css of child theme, feel free to add yours
+// This function will enqueue custom.css of child theme, feel free to add yours
 function aurum_enqueue_child_theme_scripts() {
-	wp_enqueue_style( 'aurum-child', get_stylesheet_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'aurum-child', get_stylesheet_directory_uri() . '/custom-css/custom.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'aurum_enqueue_child_theme_scripts', 100 );
